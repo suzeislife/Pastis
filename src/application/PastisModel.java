ad8e8ee547015d8542fa83693db13104e0288567
Jeremy Portelli <Baliig@air-de-valerie.sofr.hefr.lan>
2016-05-11 15:52:40 +0200

    V1.3

--- src/application/PastisModel.java
+++ src/application/PastisModel.java
@@ -1,5 +1,95 @@
 package application;
 
-public class PastisModel {
+public class PastisModel implements IPastisModel{
+
+	@Override
+	public int getLength() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void setLength(int length) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasUcLetters() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setUcLetters(boolean ucLetters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasLcLetters() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setLcLetters(boolean lcLetters) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasDigits() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setDigits(boolean digits) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean hasSymbols() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setSymbols(boolean symbols) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isPronounceable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setPronounceable(boolean pronounceable) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isUnambiguous() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setUnambiguous(boolean unambiguous) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String generateNewPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
